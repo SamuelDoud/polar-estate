fca5c4d7e21f39bc3f75d08ad64accd3de3d061f
errosica <er1093a@student.american.edu>
2016-01-30 18:50:49 -0500

    :improvements on server

--- polar-estate/server/Dserver/views.py
+++ polar-estate/server/Dserver/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from BaseHTTPServer import BaseHTTPRequestHandler
 from StringIO import StringIO
-import ast
+import ast, sys, urllib
 
 def page1(request): #name of the view = current_datetime
 	html="this is a test"
@@ -14,11 +14,18 @@ def API(request): #name of the view = current_datetime
 
 	#parse the query string 
 	start = request.find('API')
-	end = request.find("'",start)
-	query_string = request[start:end]
+	end = request.find("GET",start) - 2
+	query_string = request[start+3:end]
 
-	#this converts the string rep of the dict to 
+
+	#decode the query string: note not all ASCII characheters can be encoded into URLS
+	query_string = urllib.unquote(query_string).decode('ascii') 
+
+	query_string = query_string.encode('ascii','ignore')
+
+	#this converts the string rep of the dict to a dict
 	dict=ast.literal_eval(query_string)
+	print >> sys.stderr, "API Query: ", query_string
 
 	return HttpResponse(query_string)
 
