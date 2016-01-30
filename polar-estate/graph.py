1619a7d9502fb1780bce44b7b5a53d240ae1fa73
Samuel Doud <sd8396a@student.american.edu>
2016-01-30 03:32:10 -0500

    structured data is given per spec. use test.py to run tests on the data

--- polar-estate/graph.py
+++ polar-estate/graph.py
@@ -1,18 +1,48 @@
-﻿class graph(object):
+﻿import json
+import googlemaps
+
+class graph(object):
     """This class creates an object that represents a single graph and the data associated with that graph.
     This class does not attempt to analzye the data within it. It only offers up its display and its member's data"""
     def __init__(self, origin_address, types_of_interesting_points, max_distance):
         #max_distance does not have to be a distance, it could be a time-length
         #TODO: add an option for other transportation types (walking is clearly much different than driving
+        self.key = "AIzaSyDFGVVkUy9ISBceeA6Y02ry0Q-Yagx3CEM"
+        self.client = googlemaps.Client(self.key)
         self.address = origin_address
-        self.list_of_locations_indexed_by_type = self.get_all_locations
-    def get_all_locations(self):
-        """gets all the locations of all the types from a Google Maps API call (or set of calls). Returns a list of lists indexed by type of location"""
-        for type_of_location in self.types_of_interesting_points:
-            self.list_of_locations_indexed_by_type.append(self.get_locations(type_of_location)) #add the list of this type to the list of all lists   
-    def get_locations(super, type_of_location):
-        #get a list of a type (ex. gas station) from the origin address
-        #be sure to cull by max_distance
-    def create_polar_map(self):
-        #create a map of the radius maxdistance
+        self.types = types_of_interesting_points
+        self.radius = max_distance
+        self.list_of_locations = self.get_locations()
+
+
+    def get_locations(self):
+        matrix = []
+        for type_of in self.types:
+            matrix = self.get_locations_by_type(type_of)
+
+    def get_locations_by_type(self,type_of_place):
+        names_of_places = self.client.places(type_of_place, location=self.address, radius=self.radius)
+        print(names_of_places)
+        lat_long_pairs = []
+        total_structured_data = []
+        for place in names_of_places['results']:
+            structured_data = [type_of_place]
+            temp = place['geometry']
+            lat_long_pairs.append(temp['location'])
+            name = place['name']
+            try:
+                rating = place['rating']
+            except:
+                rating = 2.5 #middling value if it does not exist
+            structured_data.append(name)
+            structured_data.append(lat_long_pairs[len(lat_long_pairs) - 1])
+            structured_data.append(rating)
+            total_structured_data.append(structured_data)
+        locations = self.client.distance_matrix(self.address, lat_long_pairs) #this gets the real distance of the places from the location
+        row_data = locations['rows'][0]
+        for index, location in enumerate(row_data['elements']):
+            total_structured_data[index].append(location['distance']['value'])
+        print(locations)
+        print(names_of_places)
+#AIzaSyDFGVVkUy9ISBceeA6Y02ry0Q-Yagx3CEM
 
--- polar-estate/polar_estate.py
+++ polar-estate/polar_estate.py
@@ -1,3 +1,27 @@
+﻿import graph
+
 #hello team, this is the "main" file of the backend
 #I am just a holding class
 #should I be the interface for the frontend-backend communications?
+#probably a dispatcher
+class ColectionDispatch(object):
+    def __init__(self, base_address, target_addresses, types_of_locations, weights_for_types_of_locations, max_unit, time_or_distance):
+        self.address = base_address
+        self.types_of_locations = types_of_locations
+        self.target_addresses = target_addresses
+        self.weights_for_types_of_locations = weights_for_types_of_locations
+        self.max_unit = max_unit
+        self.list_of_maps = self.create_set_of_maps
+        
+    def create_set_of_maps(self):
+        list_maps_objects = []
+        for target_address in target_addresses:
+            current_graph = graph(self.address, target_address)
+            list_maps_object.append()
+            
+            
+#Distance API key    
+#AIzaSyDFGVVkUy9ISBceeA6Y02ry0Q-Yagx3CEM
+
+#Places API key
+#
