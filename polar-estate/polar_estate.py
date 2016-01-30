a3f62c524979413fa2b12366928ffdedf939f879
Samuel Doud <sd8396a@student.american.edu>
2016-01-30 05:40:33 -0500

    I'm getting tired. What changes have been made? We now can take a polar_estate object and get a tupled dict out per spec. This should be sufficient for Evan's operations. Still can't limit by distance.

--- polar-estate/polar_estate.py
+++ polar-estate/polar_estate.py
@@ -5,19 +5,19 @@
 #should I be the interface for the frontend-backend communications?
 #probably a dispatcher
 class ColectionDispatch(object):
-    def __init__(self, base_address, target_addresses, types_of_locations, weights_for_types_of_locations, max_unit, time_or_distance):
-        self.address = base_address
+    def __init__(self, base_addresses, types_of_locations, weights_for_types_of_locations, max_unit, time_or_distance):
+        self.base_addresses = base_addresses
         self.types_of_locations = types_of_locations
-        self.target_addresses = target_addresses
         self.weights_for_types_of_locations = weights_for_types_of_locations
         self.max_unit = max_unit
-        self.list_of_maps = self.create_set_of_maps
+        self.list_of_dictionaries = self.create_set_of_maps()
         
     def create_set_of_maps(self):
         list_maps_objects = []
-        for target_address in target_addresses:
-            current_graph = graph(self.address, target_address)
-            list_maps_object.append()
+        for base_address in self.base_addresses:
+            current_graph = graph.graph(base_address, self.types_of_locations, self.max_unit)
+            list_maps_objects.append((base_address, current_graph.list_of_locations)) #make this a tuple with the base address and its locations as seperate tuples
+        return list_maps_objects
             
             
 #Distance API key    
--- polar-estate/test.py
+++ polar-estate/test.py
@@ -1,5 +1,8 @@
 ﻿import graph
+import polar_estate
 
-test = graph.graph("4020 Calvert St Nw Washington DC", ["Grocery", "Bars"], 10000)
-print(test.list_of_locations)
-
+base_addresses = ["4020 Calvert St. NW Washington DC", "4400 Massachusetts AVE NW Washington DC"]
+types_of_locations = ["bars","parks", "chruches", "hospitals"]
+max_radius = 8050 #about five miles
+aggregate = polar_estate.ColectionDispatch(base_addresses, types_of_locations, [],max_radius, 0)
+print(str(aggregate.list_of_dictionaries).encode('utf-8'))
